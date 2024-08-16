a06805bee39fadbb5a6ca782b11dd8ebb7dd819b
jclasley <jon.lasley@gmail.com>
2024-08-16 18:22:48 -0400

    pretty good!

--- config.el
+++ config.el
@@ -26,7 +26,7 @@
   (when (< emacs-major-version 29)
     (error (format "Emacs Bedrock only works with Emacs 29 and newer; you have version ~a" emacs-major-version)))
 
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;in
   ;;;
   ;;;   Basic settings
   ;;;
@@ -99,6 +99,7 @@
     :ensure t
     :config
     (which-key-mode))
+
 
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;;;
@@ -249,7 +250,16 @@
 
 (setq ring-bell-function nil)
 
-(winner-mode)
+(setopt tab-width 4)
+
+(global-company-mode -1)
+
+(use-package dash :ensure t)
+
+(winner-mode 1)
+(recentf-mode 1)
+
+(eldoc-mode 1)
 ;; theme
 (add-to-list 'custom-theme-load-path "~/bedrock/themes")
 (use-package dracula-theme
@@ -268,21 +278,173 @@
   :ensure t
   :config (key-chord-mode 1))
 
-(use-package company
-  :ensure t)
+(use-package rainbow-delimiters 
+  :ensure t
+  :config (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+
+(use-package doom-modeline
+  :ensure t
+  :config (doom-modeline-mode 1))
+
+(use-package emacs
+  :custom
+  (tab-always-indent 'complete)
+  ;; Emacs 28 and newer: Hide commands in M-x which do not apply to the current
+  ;; mode.  Corfu commands are hidden, since they are not used via M-x. This
+  ;; setting is useful beyond Corfu.
+  (read-extended-command-predicate #'command-completion-default-include-p)
+  (enable-recursive-minibuffers t)
+  :init
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+            (replace-regexp-in-string
+             "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+             crm-separator)
+            (car args))
+    (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator))
+
+(use-package projectile
+  :ensure t
+  :config
+  (setq projectile-switch-project-action 'projectile-dired)
+  :bind
+  (("s-p" . #'projectile-command-map))
+  :init (projectile-mode +1))
+
+(use-package origami
+  :ensure t
+  :init (global-origami-mode))
+
+(use-package treemacs
+  :ensure t
+  :config
+  (setq treemacs-follow-mode t))
+
+(use-package eyebrowse
+  :ensure t
+  :init (eyebrowse-mode 1))
+
+(setq org-tag-alist (append '((:startgroup . nil) ; at most one of the following
+                            ("@home" . ?h)
+                            ("@work" . ?w)
+                            ("@out". ?o)
+                            (:endgroup . nil)
+                            ;; any of the following
+                            ("project" . ?p)
+                            ("learning" . ?l))
+                          org-tag-alist-for-agenda))
+
+(setq my/org-todo-keywords '(
+                             ("TODO(t)" . org-todo)
+                             ("NEXT(n)" .  (:foreground "#34ebd8" :weight bold :slant italic))
+                             ("PROG(p!)" . (:foreground "green" :weight bold))
+                             ("HOLD(h@)" . (:background "orange" :foreground "white")) ; in progress but held up
+                             ("|")
+                             ("DONE(d!)" . org-done)
+                             ("WONT(w@/!)" . (:foreground "red" :weight bold))))
 
 (use-package org
   :ensure t
+  :config
+  (setq org-agenda-files (list (expand-file-name "~/org")))
+  ;; set faces
+  (setq org-todo-keywords (list (append '(sequence)
+                                      (seq-map #'(lambda (elt)
+                                                   (if (listp elt)
+                                                       (car elt)
+                                                     elt))
+                                               my/org-todo-keywords))))
+
+(setq org-todo-keyword-faces (let ((f (lambda (elt)
+                                       (if (listp elt)
+                                           `(
+                                            ,(seq-take-while #'(lambda (elt) (not (equal ?\( elt))) (car elt))
+                                            .
+                                            ,(cdr elt))
+                                         elt)
+                                       )))
+                               (seq-map #'(lambda (elt) (funcall f elt)) my/org-todo-keywords)))
+;; 
+  ;; config files
+  (setq +org-chores-file (expand-file-name "~/org/chores.org"))
+  :bind
+  (("C-c o a" . org-agenda)
+   ("C-c o c" . org-capture))
   :init
   (add-hook 'org-mode-hook #'org-indent-mode))
 
-(defun window-map (c)
-  (interactive)
-  (which-key-show-keymap 'evil-window-map)
-  (message (apply click-key)))
-(defun click-key (c)
-  (interactive "k")
-  (c))
+(use-package org-super-agenda
+  :ensure t
+  :init (org-super-agenda-mode 1))
+
+(setq org-agenda-custom-commands
+    '(("c" "Super agenda"
+       ((agenda "" ((org-agenda-overriding-header "")
+                    (org-super-agenda-groups
+                     '((:log t)
+                       (:name "Overdue"
+                        :deadline past)
+                       (:name "Habits"
+                        :habit t)
+                       (:name "Today"
+                        :time-grid t
+                        :date today)
+                       (:name "Important"
+                        :priority "A"
+                        :face (:weight ultra-bold :background "blue")
+                        :order 1)
+                       (:name "Unstarted"
+                        :scheduled past)
+                       ))))
+        (alltodo "" ((org-agenda-overriding-header "")
+                     (org-super-agenda-groups
+                      `((:log t)
+                        (:name "Important"
+                         :priority "A"
+                         :face (:weight ultra-bold :background "blue")
+                         :order 0)
+                        (:name "Next"
+                         :todo "NEXT"
+                         :order 2)
+                       (:name "Coming up"
+                              :scheduled future)
+                        (:name "In progress"
+                         :todo "PROG"
+                         :order 1)
+                        (:name "Daphne"
+                         :tag "daphne")
+                        (:name "Chores"
+                         :file-path ,(expand-file-name +org-chores-file) ; back-quoted list allows evaluation with `,`
+                         :face (:slant italic)
+                         :order 2)
+                        (:name "Can wait"
+                         :priority "C")
+                        (:name "If time"
+                         :priority "B")
+                        (:discard (:file-path ,(expand-file-name "~/org/bills.org")))
+                        (:discard (:file-path ,(expand-file-name "~/org/habits.org")))))))))))
+
+(setq org-capture-templates '(("a" "Agenda")
+                              ("ad" "Deadline" entry
+                               (file+olp+datetree +org-capture-agenda-file)
+                               "* %?\nDEADLINE: %^{at}t")
+                              ("t" "Todos")
+                              ("tt" "project todo" entry
+                               (file "~/org/projects.org")
+                               "* TODO %?\n%i")
+                              ("tn" "today" entry
+                               (file+headline "~/org/todo.org" "Todos")
+                               "* TODO %?\n%t")
+                              ("td" "deadline" entry
+                               (file+headline "~/org/todo.org" "Todos")
+                               "* TODO %?\nDEADLINE: %^{at}t")
+                              ("ts" "scheduled" entry
+                               (file+headline "~/org/todo.org" "Todos")
+                               "* TODO %?\nSCHEDULED: %^{at}t")
+                              ("c" "chore" entry
+                               (file +org-chores-file)
+                               "* TODO %?\nDEADLINE: %t")))
 
 (use-package yasnippet
   :ensure t
@@ -295,13 +457,101 @@
 
 (use-package vertico
   :ensure t
+  :custom
+  (vertico-count 20) ;; Show more candidates
+  (vertico-resize t) ;; Grow and shrink the Vertico minibuffer
+  (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
+  :config ; evaluated after the pkg loads
+  ;; (keymap-set vertico-map "TAB" #'vertico-next)
+  ;; (keymap-set vertico-map "<backtab>" #'vertico-previous)
+  :bind
+  (("C-c '" . vertico-repeat))
   :init (vertico-mode))
+
+(use-package orderless
+  :ensure t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
 
 (use-package corfu
   :ensure t
-  :init (global-corfu-mode))
+  :custom
+  (corfu-cycle t)
+  (corfu-preselect 'prompt)
+  :config
+  (setq corfu-auto t
+        corfu-auto-prefix 2)
+  :bind
+  (:map corfu-map
+      ("TAB" . corfu-next)
+      ([tab] . corfu-next)
+      ("S-TAB" . corfu-previous)
+      ([backtab] . corfu-previous))
+  :init (global-corfu-mode 1))
 
 (use-package consult
+  :ensure t
+  :bind
+  ;; meow SPC x b
+  (("C-c s b" . consult-buffer)
+   ("C-c s l" . consult-line)
+   ("C-c s o" . consult-outline)
+   ("C-c s i" . consult-imenu)))
+
+;; Enable rich annotations using the Marginalia package
+(use-package marginalia
+  :ensure t
+  ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
+  ;; available in the *Completions* buffer, add it to the
+  ;; `completion-list-mode-map'.
+  :bind (:map minibuffer-local-map
+              ("M-A" . marginalia-cycle))
+
+  ;; The :init section is always executed.
+  :init
+
+  ;; Marginalia must be activated in the :init section of use-package such that
+  ;; the mode gets enabled right away. Note that this forces loading the
+  ;; package.
+  (marginalia-mode))
+
+(use-package embark
+  :ensure t
+
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  ;; Show the Embark target at point via Eldoc. You may adjust the
+  ;; Eldoc strategy, if you want to see the documentation from
+  ;; multiple providers. Beware that using this can be a little
+  ;; jarring since the message shown in the minibuffer can be more
+  ;; than one line, causing the modeline to move up and down:
+
+  ;; (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :ensure t ; only need to install it, embark loads it after consult if found
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
+(use-package vterm
   :ensure t)
 
 (defun meow-setup ()
@@ -328,7 +578,7 @@
    '("/" . meow-keypad-describe-key)
    '("?" . meow-cheatsheet))
   (meow-normal-define-key
-   '("0" . meow-expand-0)
+   '("0" . meow-bexpand-0)
    '("9" . meow-expand-9)
    '("8" . meow-expand-8)
    '("7" . meow-expand-7)
@@ -393,5 +643,32 @@
 (use-package meow
   :ensure t
   :config
-    (meow-setup)
-    (meow-global-mode 1))
+  (meow-setup)
+  (setq meow-use-clipboard t)
+  :init
+  (meow-global-mode 1))
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown"))
+
+(setq treesit-language-source-alist
+      '(("gomod" . "https://github.com/camdencheek/tree-sitter-go-mod")
+        ("go" . "https://github.com/tree-sitter/tree-sitter-go")))
+
+(use-package lsp-mode
+  :ensure t
+  :hook
+  ;; go
+  ((go-ts-mode . lsp-deferred)))
+
+(defun lsp-format-and-organize-imports ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+
+(add-to-list 'auto-mode-alist '("\\.go\\'" . go-ts-mode))
