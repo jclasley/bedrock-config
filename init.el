fb93ae2b8fdbe3b54e61c3f5200ecfa29dffaaac
Jon Lasley <jon.lasley@abbott.com>
2024-10-04 12:44:26 -0400

    updated!

--- init.el
+++ init.el
@@ -11,7 +11,7 @@
  '(custom-safe-themes
    '("dcb1cc804b9adca583e4e65755895ba0a66ef82d29464cf89a78b88ddac6ca53" default))
  '(package-selected-packages
-   '(templ-ts-mode dracula-theme company use-package use-package-chords key-chord evil which-key))
+   '(vertico-repeat vertico-suspend dracula-theme company use-package use-package-chords key-chord evil which-key))
  '(ring-bell-function 'ignore)
  '(tab-width 4))
 (custom-set-faces
@@ -19,4 +19,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#282a36" :foreground "#f8f8f2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 140 :width normal :foundry "nil" :family "Menlo")))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#282a36" :foreground "#f8f8f2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 140 :width normal :foundry "nil" :family "Menlo"))))
+ '(company-tooltip-annotation ((t (:foreground "dark gray"))))
+ '(match ((t (:background "light green" :foreground "#282a36"))))
+ '(region ((t (:extend nil :background "gray45")))))
