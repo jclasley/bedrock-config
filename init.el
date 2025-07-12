309e27940150b26e245ba4dc5e55833f60ec760c
Jon Lasley <jon.lasley@jupiterone.com>
2025-07-12 15:37:35 -0400

    init for diff

--- init.el
+++ init.el
@@ -1,29 +1,35 @@
 (require 'org)
 (require 'ob-tangle)
-(org-babel-load-file (expand-file-name "~/bedrock/config.org"))
-(load (expand-file-name "~/bedrock/config.el"))
+(org-babel-load-file (expand-file-name "~/emacs/bedrock-config/config.org"))
+(load (expand-file-name "~/emacs/bedrock-config/config.el"))
 
+;; macro to input the branch name at the top of the commit message
+(defalias 'start\ commit\ msg
+  (kmacro "j j j j j e e e w E y [ b g p"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("dcb1cc804b9adca583e4e65755895ba0a66ef82d29464cf89a78b88ddac6ca53" default))
+ '(org-agenda-files
+   '("~/org/projects.org" "/Users/jonlasley/.org-jira/GRAPH.org"
+     "/Users/jonlasley/org/todo.org" "/Users/jonlasley/org/habits.org"
+     "/Users/jonlasley/org/game_dev_log.org"))
+ '(org-fold-core-style 'overlays)
  '(package-selected-packages
-   '(all-the-icons dracula-theme company use-package use-package-chords key-chord evil which-key))
- '(ring-bell-function 'ignore)
- '(tab-width 4))
+   '(claude-code dracula-theme evil key-chord use-package
+		 use-package-chords which-key))
+ '(package-vc-selected-packages
+   '((claude-code :url "https://github.com/stevemolitor/claude-code.el")
+     (ultra-scroll :vc-backend Git :url
+		   "https://github.com/jdtsmith/ultra-scroll"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#282a36" :foreground "#f8f8f2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 120 :width normal :foundry "nil" :family "Menlo"))))
- '(company-tooltip-annotation ((t (:foreground "dark gray"))))
- '(match ((t (:background "plum1" :foreground "#282a36"))))
- '(region ((t (:extend nil :background "gray45")))))
-
-;; macro to input the branch name at the top of the commit message
-(defalias 'start\ commit\ msg
-  (kmacro "j j j j j e e e w E y [ b g p"))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#282a36" :foreground "#f8f8f2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 140 :width normal :foundry "nil" :family "DroidSansM Nerd Font"))))
+ '(corfu-current ((t (:extend t :background "gray30" :foreground "#f8f8f2"))))
+ '(vertical-border ((t (:background "#242530" :foreground "gray40")))))
+(put 'narrow-to-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
