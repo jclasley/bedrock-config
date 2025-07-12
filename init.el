af771b1c934dfdc6edcf38ef1c4d1a6a5e654e33
jclasley <jon.lasley@gmail.com>
2025-07-12 15:43:03 -0400

    emacs 30 changes

--- init.el
+++ init.el
@@ -8,22 +8,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("dcb1cc804b9adca583e4e65755895ba0a66ef82d29464cf89a78b88ddac6ca53" default))
+ '(org-fold-core-style 'overlays)
  '(package-selected-packages
-   '(all-the-icons dracula-theme company use-package use-package-chords key-chord evil which-key))
- '(ring-bell-function 'ignore)
- '(tab-width 4))
+   '(company dracula-theme evil key-chord paredit treesit use-package
+	     use-package-chords which-key))
+ '(package-vc-selected-packages
+   '((ultra-scroll :url "https://github.com/jdtsmith/ultra-scroll")
+     (claude-code :url
+		  "https://github.com/stevemolitor/claude-code.el"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#282a36" :foreground "#f8f8f2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 120 :width normal :foundry "nil" :family "Menlo"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#282a36" :foreground "#f8f8f2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 140 :width normal :foundry "nil" :family "DroidSansMono Nerd Font"))))
  '(company-tooltip-annotation ((t (:foreground "dark gray"))))
  '(match ((t (:background "plum1" :foreground "#282a36"))))
  '(region ((t (:extend nil :background "gray45")))))
-
-;; macro to input the branch name at the top of the commit message
-(defalias 'start\ commit\ msg
-  (kmacro "j j j j j e e e w E y [ b g p"))
